25ce8ec82b10aebc72771d241c518ba13bfb743c
driss292 <drisskaci@outlook.fr>
2022-05-18 18:05:17 +0200

    ok

--- src/App.js
+++ src/App.js
@@ -1,49 +1,13 @@
-import { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { getPosts } from "./actions/post.action";
+import { useState } from "react";
+
 import "./App.css";
 import Form from "./components/Form";
 import Header from "./components/Header";
 import TodosList from "./components/TodosList";
 
-import {
-  // addTodo,
-  getTodos,
-  // updateTodo,
-  // deleteTodo,
-  // completeTodo,
-} from "./services/todoServices";
-
 function App() {
-  // const [data, setData] = useState([]);
+  const [data, setData] = useState([]);
   const [input, setInput] = useState("");
-
-  const data = useSelector((state) => state.postReducer);
-  const store = useSelector((state) => state.store);
-
-  // useEffect(() => {
-  //   const fetchData = () => {
-  //     store.dispatch(getPosts());
-  //   };
-  //   fetchData();
-  // }, [store]);
-  // store.dispatch(getPosts());
-
-  // const dispatch = useDispatch();
-
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     try {
-  //       const response = await getTodos();
-  //       // console.log(response.data);
-
-  //       setData(response.data);
-  //     } catch (error) {
-  //       console.log(error.message);
-  //     }
-  //   };
-  //   fetchData();
-  // }, [data]);
 
   return (
     <div className="container">
@@ -55,7 +19,7 @@ function App() {
           <Form input={input} setInput={setInput} />
         </div>
         <div>
-          <TodosList data={data} />
+          <TodosList data={data} setData={setData} />
         </div>
       </div>
     </div>
